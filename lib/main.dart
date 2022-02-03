9b37f7e7e289f55ff3fb813b93846518a6fa9107
Samuel Marques <samuel.s.marques@protonmail.com>
2022-02-03 19:07:45 -0300

    Add themeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:voice_recorder_app/pages/home_page.dart';
 import 'package:voice_recorder_app/pages/search_page.dart';
 
@@ -13,13 +14,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Voice Recorder',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
       home: const HomePage(),
       routes: {
         "/search": (context) => const SearchPage(),
       },
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 14),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(style: BorderStyle.none, width: 0.0),
+          ),
+          hintStyle: GoogleFonts.getFont(
+            "Inter",
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+            color: const Color(0xFF969AA0),
+          ),
+        ),
+        tabBarTheme: TabBarTheme(
+          labelStyle: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF23262F),
+          ),
+          unselectedLabelStyle: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF23262F),
+          ),
+          labelColor: const Color(0xFF495BFF),
+          indicator: const UnderlineTabIndicator(
+            borderSide: BorderSide(width: 4.0, color: Color(0xFF495BFF)),
+          ),
+          unselectedLabelColor: const Color(0xFF23262F),
+        ),
+        textTheme: TextTheme(
+          headline1: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF23262F),
+          ),
+          headline2: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 17,
+            fontWeight: FontWeight.w500,
+            color: const Color(0xFF23262F),
+          ),
+          subtitle1: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 24,
+            fontWeight: FontWeight.w300,
+            color: const Color(0xFF777777),
+          ),
+          subtitle2: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            color: const Color(0xFF777777),
+          ),
+        ),
+      ),
     );
   }
 }
