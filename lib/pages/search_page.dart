39b057d3bd87ee5721fc8de03592b4998089e6fc
Samuel Marques <samuel.s.marques@protonmail.com>
2022-02-03 13:35:54 -0300

    Create search_page.dart

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF2F2F2),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Color(0xFF323232)),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 20.0, bottom: 15.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.start,
+                  runSpacing: 5,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Procurar",
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 14),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(style: BorderStyle.none, width: 0.0),
+                        ),
+                        suffixIcon: const Icon(
+                          Icons.search,
+                          color: Color(0xFF777777),
+                        ),
+                      ),
+                      autofocus: true,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        elevation: 0.0,
+                        color: Colors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Categorias",
+                                style: GoogleFonts.getFont(
+                                  "Inter",
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: const Color(0xFF23262F),
+                                ),
+                              ),
+                              Wrap(
+                                spacing: 10,
+                                runSpacing: -8,
+                                children: [
+                                  ActionChip(
+                                    label: Text(
+                                      "Finanças",
+                                      style: GoogleFonts.getFont(
+                                        "Inter",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: const Color(0xFF23262F),
+                                      ),
+                                    ),
+                                    backgroundColor: const Color(0xFFEFEFEF),
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                    onPressed: () {},
+                                  ),
+                                  ActionChip(
+                                    label: Text(
+                                      "Finanças",
+                                      style: GoogleFonts.getFont(
+                                        "Inter",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: const Color(0xFF23262F),
+                                      ),
+                                    ),
+                                    backgroundColor: const Color(0xFFEFEFEF),
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                    onPressed: () {},
+                                  ),
+                                  ActionChip(
+                                    label: Text(
+                                      "Finanças",
+                                      style: GoogleFonts.getFont(
+                                        "Inter",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: const Color(0xFF23262F),
+                                      ),
+                                    ),
+                                    backgroundColor: const Color(0xFFEFEFEF),
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                    onPressed: () {},
+                                  ),
+                                  ActionChip(
+                                    label: Text(
+                                      "Finanças",
+                                      style: GoogleFonts.getFont(
+                                        "Inter",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: const Color(0xFF23262F),
+                                      ),
+                                    ),
+                                    backgroundColor: const Color(0xFFEFEFEF),
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                    onPressed: () {},
+                                  ),
+                                  ActionChip(
+                                    label: Text(
+                                      "Finanças",
+                                      style: GoogleFonts.getFont(
+                                        "Inter",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: const Color(0xFF23262F),
+                                      ),
+                                    ),
+                                    backgroundColor: const Color(0xFFEFEFEF),
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                    onPressed: () {},
+                                  ),
+                                  ActionChip(
+                                    label: Text(
+                                      "Finanças",
+                                      style: GoogleFonts.getFont(
+                                        "Inter",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 14,
+                                        color: const Color(0xFF23262F),
+                                      ),
+                                    ),
+                                    backgroundColor: const Color(0xFFEFEFEF),
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
