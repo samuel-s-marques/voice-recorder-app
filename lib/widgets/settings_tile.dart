efd4e7db0c896f607b3c107c899cfe5ae8d74131
Samuel Marques <samuel.s.marques@protonmail.com>
2022-02-07 22:04:01 -0300

    Add styles

--- lib/widgets/settings_tile.dart
+++ lib/widgets/settings_tile.dart
@@ -10,9 +10,19 @@ class SettingsTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      dense: false,
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Text(title), Text(value)],
+        children: [
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headline2!.copyWith(fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+          Text(
+            value,
+            style: Theme.of(context).textTheme.subtitle2!.copyWith(fontSize: 14),
+          ),
+        ],
       ),
       onTap: onTap,
     );
