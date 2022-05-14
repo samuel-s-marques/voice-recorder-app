9a3ce48fdd8f4bce01bf27248f4582ad345453b5
Samuel Marques <samuel.s.marques@protonmail.com>
2022-05-14 14:21:46 -0300

    Remove audio tile

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -24,7 +24,6 @@ class SettingsPage extends StatelessWidget {
             children: [
               SettingsTile("Linguagem", "Português", () {}),
               SettingsTile("Modo", "Claro", () {}),
-              SettingsTile("Áudio", "", () {}),
               SettingsTile("Arquivos", "", () {}),
               SettingsTile("Lixo", "", () {}),
               SettingsTile("Privacidade", "", () {}),
